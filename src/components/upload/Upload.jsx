aaf830cea1ff70fad23bc42fd2c020cfffb23101
Austin Roy <austin.roy@andela.com>
2018-01-03 16:09:14 +0300

    Add cloudinary upload

--- src/components/upload/Upload.jsx
+++ src/components/upload/Upload.jsx
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 
 class Upload extends Component {
+
+    uploadWidget = () => {
+        window.cloudinary.openUploadWidget(
+          { cloud_name: 'austinroy',
+            upload_preset: 'unsigned',
+            tags: ['flixer'],
+            sources: ['local', 'url', 'google_photos', 'facebook', 'image_search']
+          },
+          function(error, result) {
+              console.log("This is the result of the last upload", result);
+          });
+      }
+
     render() {
         return (
             <div>
@@ -9,7 +22,7 @@ class Upload extends Component {
 
                 <div className="col-sm-12">
                     <div>
-                        <button className="button is-primary">
+                        <button className="button is-primary" onClick={this.uploadWidget}>
                         <span className="icon is-medium" style={{paddingRight: 10 }}> <i className="fa fa-upload"></i> </span>
                         Upload Video
                         </button>
